67bbc39431701ddffb200f0f0f3a7464fa34e935
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-08 01:43:01 +0100

    Update src/s3/s3_parameters.py

--- src/s3/s3_parameters.py
+++ src/s3/s3_parameters.py
@@ -50,8 +50,8 @@ class S3Parameters:
         s3_parameters = s3p.S3Parameters(**dictionary)
 
         # Parsing variables
-        region_name = self.__secret.exc(secret_id='RegionCodeDefault')
-        internal = self.__secret.exc(secret_id='InternalBucketEnqueter')
+        region_name = self.__secret.exc(secret_id=s3_parameters.region_name_identifier)
+        internal = self.__secret.exc(secret_id=s3_parameters.project_identifier, node='internal')
         s3_parameters: s3p.S3Parameters = s3_parameters._replace(
             location_constraint=region_name, region_name=region_name, internal=internal)
 
