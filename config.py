6053bc338fb564d4fd75a374f1bf62db0efb96b5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-22 21:55:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 Module config
 """
 import os
+import datetime
+import time
 
 
 class Config:
@@ -20,3 +22,24 @@ class Config:
 
         # Template
         self.s3_parameters_key = 's3_parameters.yaml'
+
+        '''
+        For configurations repository
+        '''
+
+        # Seed
+        self.seed = 5
+
+        # Span
+        self.starting = datetime.datetime.strptime('2017-01-01', '%Y-%m-%d')
+        self.at_least = datetime.datetime.strptime('2025-01-05', '%Y-%m-%d')
+
+        # Limits per access
+        self.n_stations = 9
+
+        # Period: P1D, P1M, P1Y, etc.
+        self.period = 'P1Y'
+
+        # The training/testing cut-off point
+        datestr = datetime.datetime.strptime('2025-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+        self.cutoff = 1000 * time.mktime(datestr.timetuple())
