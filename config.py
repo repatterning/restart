010a4cff45d09b0ab7400aa168bd550a82ce40a0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-22 16:01:55 +0000

    Update config.py

--- config.py
+++ config.py
@@ -19,15 +19,5 @@ class Config:
 
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
 
-        # A S3 parameters template
-        self.s3_parameters_template = 'https://raw.githubusercontent.com/enqueter/.github/master/profile/s3_parameters_sandbox.yaml'
-
-        # After the development phase the start date will be a few years earlier.
-        self.starting = datetime.datetime.strptime('2018-01-01', '%Y-%m-%d')
-        self.ending = datetime.datetime.today() - datetime.timedelta(days=3)
-
-        # Devices in focus, via their sequence identifiers
-        # pollutant: Nitrogen Dioxide
-        # area: Edinburgh
-        # sequence (station): 155 (901), 531 (1014), 177 (460), 150 (791)
-        self.sequence_id_filter = [155, 531, 177, 150]
+        # Template
+        self.s3_parameters_key = 's3_parameters.yaml'
