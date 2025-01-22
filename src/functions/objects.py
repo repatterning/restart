e460bec9616ff6571ee8b087d9a422e0190ed092
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-22 01:10:00 +0000

    Update src/functions/objects.py

--- src/functions/objects.py
+++ src/functions/objects.py
@@ -1,10 +1,11 @@
 """
 Module objects.py
 """
-
 import json
 import pathlib
-import requests
+import pandas as pd
+
+import src.functions.api
 
 
 class Objects:
@@ -25,8 +26,8 @@ class Objects:
     def write(nodes: dict, path: str) -> str:
         """
 
-        :param nodes:
-        :param path:
+        :param nodes: A dictionary of data
+        :param path: A string, which includes a file name & extension, representing a storage point.
         :return:
         """
 
@@ -46,27 +47,20 @@ class Objects:
     def api(url: str) -> dict:
         """
 
-        :param url:
+        :param url: An online data source URL (Uniform Resource Locator)
         :return:
         """
 
-        try:
-            response = requests.get(url=url, timeout=600)
-            response.raise_for_status()
-        except requests.exceptions.Timeout as err:
-            raise f"Time Out:\n{err}" from err
-        except Exception as err:
-            raise err from err
+        instance = src.functions.api.API()
+        content = instance(url=url)
 
-        if response.status_code == 200:
-            return response.json()
-        raise f'Failure code: {response.status_code}'
+        return json.loads(content)
 
     @staticmethod
     def read(uri: str) -> dict:
         """
 
-        :param uri:
+        :param uri: A file's URI (Uniform Resource Identifier)
         :return:
         """
 
@@ -75,3 +69,19 @@ class Objects:
                 return json.load(fp=disk)
         except ImportError as err:
             raise err from err
+
+    @staticmethod
+    def frame(path: str, orient: str) -> pd.DataFrame:
+        """
+
+        :param path:
+        :param orient:
+        :return:
+        """
+
+        try:
+            cases = pd.read_json(path_or_buf=path, orient=orient)
+        except ImportError as err:
+            raise err from err
+
+        return cases
