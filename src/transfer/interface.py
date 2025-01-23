53595b7ce727f478308569b8b3a0679b5e02bcf4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-23 22:42:17 +0000

    Update src/transfer/interface.py: Temporary Edit

--- src/transfer/interface.py
+++ src/transfer/interface.py
@@ -41,10 +41,10 @@ class Interface:
         logging.info(strings)
 
         # Transfer
-        if not strings.empty:
-            messages = src.s3.ingress.Ingress(
-                service=self.__service, bucket_name=self.__s3_parameters.internal).exc(
-                strings=strings, tags={'project': 'hydrography'})
-            logging.info(messages)
-        else:
-            logging.info('Empty')
+        # if not strings.empty:
+        #     messages = src.s3.ingress.Ingress(
+        #         service=self.__service, bucket_name=self.__s3_parameters.internal).exc(
+        #         strings=strings, tags={'project': 'hydrography'})
+        #     logging.info(messages)
+        # else:
+        #     logging.info('Empty')
