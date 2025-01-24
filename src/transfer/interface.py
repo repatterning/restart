ef3507b4221e9068ed54a7ec19e71bf0d66e3d7b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-24 06:18:51 +0000

    Update src/transfer/interface.py

--- src/transfer/interface.py
+++ src/transfer/interface.py
@@ -1,9 +1,9 @@
 """Module interface.py"""
+import json
 import logging
 import os
 
 import pandas as pd
-import json
 
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
@@ -57,10 +57,10 @@ class Interface:
         logging.info(strings)
 
         # Transfer
-        # if not strings.empty:
-        #     messages = src.s3.ingress.Ingress(
-        #         service=self.__service, bucket_name=self.__s3_parameters.internal).exc(
-        #         strings=strings, tags={'project': 'hydrography'})
-        #     logging.info(messages)
-        # else:
-        #     logging.info('Empty')
+        if not strings.empty:
+            messages = src.s3.ingress.Ingress(
+                service=self.__service, bucket_name=self.__s3_parameters.internal).exc(
+                strings=strings, tags={'project': 'hydrography'})
+            logging.info(messages)
+        else:
+            logging.info('Empty')
