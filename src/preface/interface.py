03bddce0cc318937e32a58afebba16a82980fce0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-24 14:57:46 +0000

    Add src/preface/interface.py

---
+++ src/preface/interface.py
@@ -0,0 +1,50 @@
+"""Module interface.py"""
+import logging
+import typing
+
+import boto3
+
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import src.functions.service
+import src.preface.setup
+import src.s3.configurations
+import src.s3.s3_parameters
+
+
+class Interface:
+    """
+    Interface
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __get_attributes(connector: boto3.session.Session) -> dict:
+        """
+
+        :return:
+        """
+
+        key_name = 'attributes.json'
+
+        return src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
+
+    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
+        """
+
+        :return:
+        """
+
+        connector = boto3.session.Session()
+        s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
+        service: sr.Service = src.functions.service.Service(
+            connector=connector, region_name=s3_parameters.region_name).exc()
+        attributes: dict = self.__get_attributes(connector=connector)
+
+        setup = src.preface.setup.Setup(
+            service=service, s3_parameters=s3_parameters).exc(reacquire=attributes['reacquire'])
+        logging.info('Successful Set Up: %s', 'Yes' if setup else 'No')
+
+        return connector, s3_parameters, service, attributes
