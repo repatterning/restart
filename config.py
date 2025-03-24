0924b6745fc3ed5cb88829f2d19bd0be2889f654
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-24 17:16:14 +0000

    Update config.py

--- config.py
+++ config.py
@@ -24,28 +24,3 @@ class Config:
 
         # Template
         self.s3_parameters_key = 's3_parameters.yaml'
-
-
-        '''
-        For the acquisition configurations json/yaml
-        '''
-
-        # Seed
-        self.seed = 5
-
-        # Span
-        self.starting = datetime.datetime.strptime('2017-01-01', '%Y-%m-%d')
-        self.at_least = datetime.datetime.strptime('2025-01-05', '%Y-%m-%d')
-
-        # Period
-        self.period = 'P1Y'
-
-        # The training/testing cut-off point
-        datestr = datetime.datetime.strptime('2025-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-        self.cutoff = 1000 * time.mktime(datestr.timetuple())
-
-        # Reacquire all again
-        self.reacquire = False
-
-        # If not re-acquiring, a set of specific time series in focus; via time series codes
-        self.specific = [57532010, 52488010]
