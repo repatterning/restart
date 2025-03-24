2895a769b90238ed4155c2463dbdfce0c79892f5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-24 14:14:02 +0000

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ class Config:
         self.cutoff = 1000 * time.mktime(datestr.timetuple())
 
         # Reacquire all again
-        self.reacquire = True
+        self.reacquire = False
 
         # If not re-acquiring, a set of specific time series in focus; via time series codes
         self.specific = [57532010, 52488010]
